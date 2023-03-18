0fececeb92383a71e624214aa3b6d1eeb64e901b
Chowański Sławomir <chowanski@wp.pl>
2023-03-18 10:46:39 +0100

    Add file with thernary operator

---
+++ PHP7 i SQL/Lekcja_07/plik_a.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ * plik_a.php
+ * Copyright 2023 Sławomir Chowański
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ */
+/* definicja zmiennych */
+$a = 15;
+$b = 8;
+
+($a > $b) ? print "a jest większe od b" : print "a jest mniejsze od  b" ;
+?>
