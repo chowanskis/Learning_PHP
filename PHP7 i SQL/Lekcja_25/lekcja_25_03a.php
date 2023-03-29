6dae3ebe5f5e4f37a4ab6b33f736b28150fd351b
Chowański Sławomir <chowanski@wp.pl>
2023-03-29 18:35:00 +0200

    Add files to repo

---
+++ PHP7 i SQL/Lekcja_25/lekcja_25_03a.php
@@ -0,0 +1,40 @@
+<?php
+class Produkt // definicja klasy
+{
+    private $nazwa; // właściwość dostępna lokalnie
+    private $brutto; // i ta również
+    private $netto; // i jeszcze ta
+    
+    // Dlaczego ta wartość dosępna jest pubicznie?
+    // Po co udostepniać ja publicznie? 
+    // Tego ie da się wykorzystać!
+    public $vat = 23; // właściwość dostępna publicznie
+    
+    public function __construct($nazwa, $brutto) // konstruktor
+    {
+        $this->nazwa = $nazwa;
+        $this->brutto = $brutto;
+        $this->netto = $this->setNetto(); // wywołaj metodę lokalną
+    }
+    
+    public function drukuj() // metoda dostępna publicznie
+    {
+        printf(
+        "Produkt: %s\nCena netto: %.2f PLN\nPodatek: %.2f %%\nCena brutto: %.2f PLN",
+        $this->nazwa,
+        $this->netto,
+        $this->vat,
+        $this->brutto
+        );
+    }
+    
+    private function setNetto() // metoda dostępna lokalnie
+    {
+        return $this->brutto - ($this->brutto / (1 + $this->vat));
+    }
+}
+
+$owoc = new Produkt("Jabłko", 4); // tworzenie egzemplarza klasy
+$owoc->vat = 7;
+print $owoc->drukuj(); // wywołanie metody publicznej
+?>
